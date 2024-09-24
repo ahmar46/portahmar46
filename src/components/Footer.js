b7695ff8e8f9a192f6f20be6183dd103bb6c244e
Ahmar <mail2ahmaralikhan52@gmail.com>
2024-09-24 23:52:52 +0530

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>❤️ handcrafted with love</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} ahmar46</h3>
+          <h3>Copyright © {year} onlyahmar</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
