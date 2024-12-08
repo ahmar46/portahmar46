4ad8ca3e2568e39ec579d85a4bab42c1000cd944
Ahmar <mail2ahmaralikhan52@gmail.com>
2024-12-08 23:01:04 +0530

    Prohjects 001

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -56,8 +56,8 @@ function Projects() {
             <ProjectCard
               imgPath={CDC}
               isBlog={false}
-              title="Crep Dog Crew"
-              description="Developed a fully customized Shopify store for Crepdogcrew, focusing on creating a user-friendly shopping experience with optimized performance, responsive design, and seamless integrations for payment gateways and inventory management."
+              title="StepArtSole"
+              description="lorem ipsum hgsdfhgsdhjfgsdhj"
               // ghLink="https://github.com/soumyajit4419/Editor.io"
               demoLink="https://crepdogcrew.com/"              
             />
